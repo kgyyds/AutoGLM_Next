0aef6f686712f0f31fa5a2f835d60e73f8b65aee
kgyyds <1361906712@qq.com>
2025-12-18 11:23:23 +0800

    更改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 103
-        versionName = "1.0.3"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
